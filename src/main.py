336861f9886af07da262cfcc325a8464af38ccd9
artmtt <aaaajjjj72@gmail.com>
2024-11-26 15:16:51 -0600

    Added CORS

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import search_router, random_router
 
 load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / '.env')
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 app.include_router(search_router, prefix='/api/search', tags=['search'])
